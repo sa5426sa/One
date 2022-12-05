0919c753aabdd126ae5ab0a117c47fd3dd35df63
Shaked <sa5426@bs.amalnet.k12.il>
2022-12-05 11:10:42 +0200

    initial commit

---
+++ app/src/main/java/com/example/one/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.one;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.btn);
+    }
+
+    public void clicked(View view) {
+        btn.setText("Oh, yea, I've been clicked!");
+    }
+}
